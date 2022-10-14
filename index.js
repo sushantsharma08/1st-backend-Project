5edde4e88cfc17f05d4b067c12bef8822e18a4f6
Sushant Sharma <ssushantlb08@gmail.com>
2022-10-14 11:23:58 +0530

    Basic api creation

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url')
+
+
+
+const port = 8000;
+const server = http.createServer((req,res)=>{
+    const pathName = req.url;
+    console.log(req.url);
+    if(pathName==='/'||pathName==="Dashboard"){
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.end(read)
+    }
+})
+server.listen(port  ,'127.0.0.1', (err)=>{
+    console.log(`listening to port ${port}`);
+   });
+const read = fs.readFileSync('./source/dataSets/studentData.json','utf-8');
+
+console.log(read);
