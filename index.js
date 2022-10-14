8841b4453abf4c52dc5952b1b2fafb58ff265dbe
Sushant Sharma <ssushantlb08@gmail.com>
2022-10-15 01:39:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ const server = http.createServer((req,res)=>{
     const pathName = req.url;
     console.log(req.url);
     if(pathName==='/'||pathName==="Dashboard"){
+        res.end('Welcome to the Server')
+    }
+    else if(pathName==='/studentapi'){
         res.writeHead(200,{'Content-type':'text/html'})
         res.end(read)
     }
+    
 })
 server.listen(port  ,'127.0.0.1', (err)=>{
     console.log(`listening to port ${port}`);
